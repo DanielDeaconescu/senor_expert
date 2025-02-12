f19d94565b094493cd17a93613a0436f862c6724
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-02-12 19:34:34 +0200

    further changes

--- src/ui/UploadSteps.jsx
+++ src/ui/UploadSteps.jsx
@@ -7,45 +7,67 @@ const StyledUploadSteps = styled.div`
 `;
 
 const ContainerSteps = styled.div`
+  gap: 1.2rem;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1.2rem;
 `;
 
 const Text = styled.div``;
+
+const Step = styled.div`
+  /* display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) and (max-width: 1400px) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  } */
+`;
+
+const SmallElement = styled.small`
+  display: flex;
+  justify-content: center;
+  width: 45%;
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+`;
 
 function UploadSteps() {
   return (
     <StyledUploadSteps className="container-fluid">
       <div className="container">
-        <ContainerSteps className="row vstack">
-          <div className="col-md-4">
+        <ContainerSteps className="row vstack text-center">
+          <Step className="col-sm-12">
             <h4>
               <i class="bi bi-1-circle-fill pe-2"></i>
               Apasă butonul "Încarcă documente"
             </h4>
             {/* <Text>Apasă butonul "Încarcă documente" de mai jos.</Text> */}
-          </div>
-          <div className="col-md-4">
+          </Step>
+          <Step className="col-sm-12">
             <h4>
               <i class="bi bi-2-circle-fill pe-2"></i>
               Completează formularul cu atenție
             </h4>
-            <small>
+            <SmallElement className="m-auto">
               <span className="pe-2">
                 <i class="fa-solid fa-circle-info"></i>
               </span>
               Pentru a încărca mai multe documente simultan, țineți apăsat
               butonul "Ctrl" (butonul de Control din partea de stânga jos a
               tastaturii) la selectarea documentelor.
-            </small>
-          </div>
-          <div className="col-md-4">
+            </SmallElement>
+          </Step>
+          <Step className="col-sm-12">
             <h4>
               <i class="bi bi-3-circle-fill pe-2"></i>
               Apasă butonul "Trimite"
             </h4>
-          </div>
+          </Step>
         </ContainerSteps>
       </div>
     </StyledUploadSteps>
