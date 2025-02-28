3c7ef7aac66340e7d3c4f91395b397efcfe5200d
danieldeaconescu <daniel.deaconescu23@gmail.com>
2025-02-28 13:06:27 +0200

    further changes

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,11 +1,25 @@
 import { NavLink } from "react-router";
+import { useState } from "react";
 import styled from "styled-components";
+import AuthorizationImage from "../data/images/autorizatie-ceccar.jpg";
 
 const StyledFooter = styled.footer`
   border-top: 4px solid var(--clr-primary);
 `;
 
-const TableContainer = styled.div``;
+// const CopyrightText = styled.p`
+
+// `;
+
+const CopyrightSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 992px) {
+    align-items: flex-start;
+  }
+`;
 
 const NavListFooter = styled.ul`
   display: flex;
@@ -17,54 +31,256 @@ const NavListFooter = styled.ul`
 `;
 
 const StyledNavLink = styled(NavLink)`
-  &:hover {
-    text-decoration: underline var(--clr-primary) !important;
-    text-decoration-thickness: 3px;
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  font-weight: 500;
+  transition: color 0.3s ease-in-out;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 100%;
+    height: 2px;
+    background-color: currentColor;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  @media (min-width: 768px) {
+    &:hover {
+      color: var(--clr-primary);
+    }
+
+    &:hover::after {
+      transform: scaleX(1);
+    }
   }
 `;
 
+const StyledEmailLink = styled(StyledNavLink).attrs({ as: "a" })``;
+
+const PhoneButton = styled.button`
+  background: none;
+  border: none;
+  color: inherit;
+  font-weight: bold;
+  cursor: pointer;
+  padding: 0;
+  font-size: 1rem;
+  position: relative;
+  transition: color 0.3s ease-in-out;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 100%;
+    height: 2px;
+    background-color: currentColor;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover {
+    color: var(--clr-primary);
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+`;
+
+const StyledPhoneLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  font-weight: 500;
+  transition: color 0.3s ease-in-out;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 100%;
+    height: 2px;
+    background-color: currentColor;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover {
+    color: var(--clr-primary);
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+`;
+
+const CopyrightText = styled.p`
+  font-weight: normal; // Or use a specific value like 400 or 500 based on your design
+  font-size: 1rem; // Adjust the font size if necessary
+  margin: 0;
+  text-align: right;
+  color: var(--clr-dark);
+`;
+
+const TableContainer = styled.div``;
+
+const DanielDevLink = styled.a`
+  text-decoration: underline;
+`;
+
 function Footer() {
+  const [phoneVisible, setPhoneVisible] = useState(false);
+
   return (
     <StyledFooter className="bg-light text-dark py-4">
       <div className="container">
         <div className="row">
+          {/* Quick Links */}
           <div className="col-md-4">
-            <h3>Link-uri utile</h3>
-            <NavListFooter className="nav-list-footer">
-              <li className="nav-item">
-                <StyledNavLink to="/" className="nav-link link-dark">
-                  Acasă
-                </StyledNavLink>
+            <h5>Link-uri utile</h5>
+            <NavListFooter>
+              <li>
+                <StyledNavLink to="/">Acasă</StyledNavLink>
               </li>
-              <li className="nav-item">
-                <StyledNavLink to="/services" className="nav-link link-dark">
-                  Servicii
-                </StyledNavLink>
+              <li>
+                <StyledNavLink to="/services">Servicii</StyledNavLink>
               </li>
-              <li className="nav-item">
-                <StyledNavLink to="/connect" className="nav-link link-dark">
-                  Conectare
-                </StyledNavLink>
+              <li>
+                <StyledNavLink to="/connect">Conectare</StyledNavLink>
               </li>
-              <li className="nav-item">
+              <li>
                 <StyledNavLink
                   data-bs-toggle="modal"
                   data-bs-target="#pricesList"
-                  className="nav-link link-dark"
                 >
                   Lista de prețuri
                 </StyledNavLink>
               </li>
+              <li>
+                <StyledNavLink
+                  data-bs-toggle="modal"
+                  data-bs-target="#authorizationModal"
+                  as="a"
+                  href="/certificat-ceccar.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Certificat CECCAR
+                </StyledNavLink>
+              </li>
+
+              <div
+                class="modal fade"
+                id="authorizationModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">
+                        Autorizație C.E.C.C.A.R.
+                      </h5>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      <img
+                        src={AuthorizationImage}
+                        alt=""
+                        className="img-fluid"
+                      />
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Închide
+                      </button>
+                      <a
+                        href="../data/pdfs/autorizatie-ceccar.pdf"
+                        download="autorizatie-ceccar.pdf"
+                        class="btn btn-primary"
+                      >
+                        Descarcă PDF
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </NavListFooter>
           </div>
-          <div className="col-md-4"></div>
+
+          {/* Contact Section */}
           <div className="col-md-4">
-            <p className="text-end d-none d-md-block">
-              Toate drepturile rezervate &copy; 2025 - Senor Expert
+            <h5>Contact</h5>
+            <p>
+              <strong>Email:</strong>{" "}
+              <StyledEmailLink href="mailto:stefan.1mihai@yahoo.com">
+                stefan.1mihai@yahoo.com
+              </StyledEmailLink>
+            </p>
+            <p>
+              <strong>Telefon:</strong>{" "}
+              {phoneVisible ? (
+                <StyledPhoneLink href="tel:+40751159264">
+                  +40 751 159 264
+                </StyledPhoneLink>
+              ) : (
+                <PhoneButton onClick={() => setPhoneVisible(true)}>
+                  Click pentru a vedea
+                </PhoneButton>
+              )}
+            </p>
+            <p>
+              <strong>Program:</strong> Luni-Vineri: 09:00 - 17:00
             </p>
           </div>
+
+          {/* Copyright Section */}
+          <CopyrightSection className="col-md-4">
+            <h5>Drepturi de autor & Credite</h5>
+            <CopyrightText className="text-end">
+              Toate drepturile rezervate &copy; {new Date().getFullYear()}
+            </CopyrightText>
+            <CopyrightText className="text-end">
+              <NavLink to="/privacy-policy" className="text-dark">
+                Politica de confidențialitate
+              </NavLink>
+            </CopyrightText>
+
+            <CopyrightText className="text-end ">
+              Website realizat de{" "}
+              <DanielDevLink
+                href="https://danieldeaconescu.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Daniel Dev
+              </DanielDevLink>
+            </CopyrightText>
+            <CopyrightText className="text-end">
+              Senor Expert | Servicii de contabilitate
+            </CopyrightText>
+          </CopyrightSection>
         </div>
       </div>
+
       <div
         class="modal fade modal-prices-list"
         id="pricesList"
