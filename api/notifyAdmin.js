2da1ccd97244d7f82ce6aa40aca79147f7e3cb19
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-03-18 17:13:41 +0200

    further changes

--- api/notifyAdmin.js
+++ api/notifyAdmin.js
@@ -11,6 +11,8 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Missing required fields" });
   }
 
+  console.log({ companyName, month, fileNames });
+
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
