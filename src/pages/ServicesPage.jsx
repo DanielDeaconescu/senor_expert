1fbd27f9f635fb3126dd0a51c5ce18e076c37518
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-03-27 21:42:59 +0200

    further changes

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -10,7 +10,7 @@ function ServicesPage() {
     <>
       <ServicesHeader />
       <TabbedComponentServices />
-      <FullServicesList />
+      {/* <FullServicesList /> */}
       <CallToActionServices />
       <SideButtons />
     </>
